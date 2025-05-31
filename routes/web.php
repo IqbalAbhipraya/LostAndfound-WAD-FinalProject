f74f4b4ba09f7b931f3e1d65d79f14f8906eedb2
gia150905 <giaputripratiwi@gmail.com>
2025-05-31 19:24:36 +0700

    for web and migration table lost item

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-
 use App\Http\Controllers\LostItemController;
-
 use App\Http\Controllers\FoundItemController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReturnReportController;
@@ -16,18 +14,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Public routes for lost items
-Route::get('/lost-items', [LostItemController::class, 'index'])->name('lost-items.index');
-Route::get('/lost-items/details/{id}', [LostItemController::class, 'show'])->name('lost-items.show');
-Route::get('/lost-items/create', [LostItemController::class, 'create'])->name('lost-items.create');
-Route::post('/lost-items', [LostItemController::class, 'store'])->name('lost-items.store');
+
 
 // Authenticated routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 
     // Found items resource routes (if you still need them)
     Route::resource('/founditems', FoundItemController::class);
@@ -39,6 +32,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/founditems/create', [FoundItemController::class, 'create'])->name('found.create');
     Route::post('/founditems', [FoundItemController::class, 'store'])->name('found.store');
 
+    Route::get('/lost-items', [LostItemController::class, 'index'])->name('lost-items.index');
+    Route::get('/lost-items/details/{id}', [LostItemController::class, 'show'])->name('lost-items.show');
+    Route::get('/lost-items/create', [LostItemController::class, 'create'])->name('lost-items.create'); 
+    Route::post('/lost-items', [LostItemController::class, 'store'])->name('lost-items.store');
+
+    Route::get('/lost-items/{id}/edit', [LostItemController::class, 'edit'])->name('lost-items.edit');
+    Route::put('/lost-items/{id}', [LostItemController::class, 'update'])->name('lost-items.update');
+    Route::delete('/lost-items/{id}', [LostItemController::class, 'destroy'])->name('lost-items.destroy');
+    
 });
 
 require __DIR__.'/auth.php';
