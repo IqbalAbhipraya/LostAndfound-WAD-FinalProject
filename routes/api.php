33e77df28617faa546fae163a09e9958e7cea3e6
IqbalAbhipraya <abhipraya3105@gmail.com>
2025-06-03 13:47:45 +0700

    api benerin

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,23 @@
 <?php
 
-
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ReturnReportApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FoundItemApiController;
 
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/signin', function (Request $request) {
+    return response()->json(['message'=>'Unauthorized'], 401);
+});
+
+Route::post('/signup',[AuthController::class, 'signup']);
+Route::post('/signin',[AuthController::class, 'signin'])->name('signin');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/returnReport', [ReturnReportApiController::class, 'index']);
+    Route::get('/founditems', [FoundItemApiController::class, 'indexApi']);
+});
 
 
-Route::get('/returnReport', [ReturnReportApiController::class, 'index']);
-
-Route::get('/founditems', [FoundItemApiController::class, 'indexApi']);
 
